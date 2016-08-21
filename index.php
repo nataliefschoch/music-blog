03785773cfb9afcb309e4ce73b9309be40798c66
Natalie Schoch <natalie@zenpayroll.com>
2016-08-21 12:27:31 -0700

    added graph footer and added styling to jquery shadow effect

--- index.php
+++ index.php
@@ -16,125 +16,112 @@
 	<header>
 	<div class='container'>
 		<div class='nav row'>
-			<div class='col-md-6'>
+			<div class='col-md-4'>
 				<h1 class='caps'>
-					I need space
+					<a href='#'>I need space</a>
 				</h1>
 			</div>
-			<div class='col-md-6'>
+			<div class='col-md-2 text-right'>
+				<button class='btn btn-secondary' type="button">
+					bookmark
+				</button>
+			</div>
+			<div class='col-md-5'>
 				<div class="input-group">
 		      <input type="text" class="form-control" placeholder="Enter your email...">
 		      <span class="input-group-btn">
-		        <button class="btn btn-secondary" type="button">subscribe</button>
+		        <button class="btn btn-primary" type="button">subscribe</button>
 		      </span>
 		    </div>
 			</div>
 		</div>
 	</div>
-</header>
-<section class='content text-center'>
-	<div class='container'>
-		<div class='row'>
-			<div class='col-md-4'>
-				<div class='row'>
-					<div class='col-md-12'>
-						<a href='#'>
-							<div class='album-wrapper'>
-								<div class='album'><img src='images/album-cover-soft-tender@2x.jpg' width='325px'></div>
-								<div class='album-shadow'></div>
-							</div>
-						</a>
-					</div>
-					<div class='col-md-12'>
-						<a href='#'>
-							<div class='album-wrapper'>
-								<div class='album'></div>
-								<div class='album-shadow'></div>
-							</div>
-						</a>
-					</div>
+	</header>
+	<section class='content text-center'>
+		<div class='container'>
+			<div class='row'>
+				<div class='col-md-4'>
+					<a href='#'>
+						<div class='album-wrapper'>
+							<div class='album'><img src='images/album-cover-soft-tender@2x.jpg' width='325px'></div>
+							<div class='album-shadow'></div>
+						</div>
+					</a>
+					<a href='#'>
+						<div class='album-wrapper'>
+							<div class='album'></div>
+							<div class='album-shadow'></div>
+						</div>
+					</a>
+				</div>
+				<div class='col-md-4'>
+					<a href='#'>
+						<div class='album-wrapper'>
+							<div class='album'></div>
+							<div class='album-shadow'></div>
+						</div>
+					</a>
+					<a href='#'>
+						<div class='album-wrapper'>
+							<div class='album'></div>
+							<div class='album-shadow'></div>
+						</div>
+					</a>
+				</div>
+				<div class='col-md-4'>
+					<a href='#'>
+						<div class='album-wrapper'>
+							<div class='album'></div>
+							<div class='album-shadow'></div>
+						</div>
+					</a>
+					<a href='#'>
+						<div class='album-wrapper'>
+							<div class='album'></div>
+							<div class='album-shadow'></div>
+						</div>
+					</a>
 				</div>
 			</div>
-			<div class='col-md-4'>
-				<div class='row'>
-					<div class='col-md-12'>
-						<a href='#'>
-							<div class='album-wrapper'>
-								<div class='album'></div>
-								<div class='album-shadow'></div>
-							</div>
-						</a>
-					</div>
-					<div class='col-md-12'>
-						<a href='#'>
-							<div class='album-wrapper'>
-								<div class='album'></div>
-								<div class='album-shadow'></div>
-							</div>
-						</a>
-					</div>
+		</div>
+	</section>
+	<footer>
+		<div class='container'>
+			<div class='row'>
+				<div class='col-md-4'>
+					<p>
+						I need space is a curated list of chill songs for chill people.
+						Curated by Natalie
+					</p>
 				</div>
-			</div>
-			<div class='col-md-4'>
-				<div class='row'>
-					<div class='col-md-12'>
-						<a href='#'>
-							<div class='album-wrapper'>
-								<div class='album'></div>
-								<div class='album-shadow'></div>
-							</div>
-						</a>
+				<div class='col-md-8'>
+					<div class='social-links'>
+						<div class='social'>
+							<a href='#'>Spotify</a>
+						</div>
 					</div>
-					<div class='col-md-12'>
-						<a href='#'>
-							<div class='album-wrapper'>
-								<div class='album'></div>
-								<div class='album-shadow'></div>
-							</div>
-						</a>
+					<div class='social-links'>
+						<div class='social'>
+							<a href='#'>Email</a>
+						</div>
+					</div>
+					<div class='social-links'>
+						<div class='social'>
+							<a href='#'>Insta</a>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-</section>
-<footer>
-	<div class='container'>
-		<div class='row'>
-			<div class='col-md-4'>
-				<p>
-					I need space is a curated list of chill songs for chill people.
-					Curated by Natalie
-				</p>
-			</div>
-			<div class='col-md-8'>
-				<div class='social-links'>
-					<div class='social'>
-						<a href='#'>Spotify</a>
-					</div>
-				</div>
-				<div class='social-links'>
-					<div class='social'>
-						<a href='#'>Email</a>
-					</div>
-				</div>
-				<div class='social-links'>
-					<div class='social'>
-						<a href='#'>Insta</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</footer>
+	</footer>
 </body>
 
 <script>
-	$( ".album" ).hover(
+	$(".album").hover(
 	  function() {
-	    $( '.album-shadow' ).css( "transform","translate(0px, 0px)" );
+	    $('.album-shadow').css("transform","translate(25px,25px)").addClass('box-shadow');
 	  }, function() {
-	    $('.album-shadow').css( "transform","translate(20px, 20px)" );
+	    $('.album-shadow').css("transform","translate(20px, 20px)").removeClass('box-shadow');
 	  }
 	);
 </script>
