69363fdeef261fa7c8458e324d8c28b0898656b1
Natalie Schoch <natalie@zenpayroll.com>
2016-08-21 10:58:50 -0700

    added foundation to music blog

---
+++ index.php
@@ -0,0 +1,111 @@
+
+<!DOCTYPE html>
+<head>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css" TYPE="text/css">
+  <link rel="icon" type="image/png" href="images/favicon.ico"/>
+  <title>I Need Space</title>
+  <meta name="keywords" content="natalie, schoch, designer, graphic, illustration, infographics">
+  <meta name="description" content="Natalie Schoch - Portfolio">
+  <meta name="author" content="Natalie Schoch">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+	<header>
+	<div class='container'>
+		<div class='nav row'>
+			<div class='col-md-6'>
+				<h1 class='caps'>
+					I need space
+				</h1>
+			</div>
+			<div class='col-md-6'>
+				<div class="input-group">
+		      <input type="text" class="form-control" placeholder="Enter your email...">
+		      <span class="input-group-btn">
+		        <button class="btn btn-secondary" type="button">subscribe</button>
+		      </span>
+		    </div>
+			</div>
+		</div>
+	</div>
+</header>
+<section class='content text-center'>
+	<div class='container'>
+		<div class='row'>
+			<div class='col-md-4'>
+				<div class='row'>
+					<div class='col-md-12'>
+						<a href='#'>
+							<div class='album'><img src='images/album-cover-soft-tender@2x.jpg' width='325px'></div>
+						</a>
+					</div>
+					<div class='col-md-12'>
+						<a href='#'>
+							<div class='album'></div>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class='col-md-4'>
+				<div class='row'>
+					<div class='col-md-12'>
+						<a href='#'>
+							<div class='album'></div>
+						</a>
+					</div>
+					<div class='col-md-12'>
+						<a href='#'>
+							<div class='album'></div>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class='col-md-4'>
+				<div class='row'>
+					<div class='col-md-12'>
+						<a href='#'>
+							<div class='album'></div>
+						</a>
+					</div>
+					<div class='col-md-12'>
+						<a href='#'>
+							<div class='album'></div>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<footer>
+	<div class='container'>
+		<div class='row'>
+			<div class='col-md-4'>
+				<p>
+					I need space is a curated list of chill songs for chill people.
+					Curated by Natalie
+				</p>
+			</div>
+			<div class='col-md-8'>
+				<div class='social-links'>
+					<div class='social'>
+						<a href='#'>Spotify</a>
+					</div>
+				</div>
+				<div class='social-links'>
+					<div class='social'>
+						<a href='#'>Email</a>
+					</div>
+				</div>
+				<div class='social-links'>
+					<div class='social'>
+						<a href='#'>Insta</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+</body>
