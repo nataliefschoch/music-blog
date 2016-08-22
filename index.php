ae7a5ec205a49d8e147d635c218350f03c670e3a
Natalie Schoch <natalie@zenpayroll.com>
2016-08-21 20:21:46 -0700

    added tinypass email form and updated album img

--- index.php
+++ index.php
@@ -3,11 +3,28 @@
 	<section class='content content-home text-center'>
 		<div class='container'>
 			<div class='row'>
-				<div class='col-md-4'>
+				<div class='col-md-4 col-sm-6'>
 					<a href='playlist-soft-tender-2016.php'>
 						<div class='album-wrapper'>
 							<div class='album'>
 								<img src='images/album-cover-soft-tender@2x.jpg' width='325px'>
+							</div>
+							<div class='album-shadow'></div>
+						</div>
+					</a>
+					<a href='#'>
+						<div class='album-wrapper'>
+							<div class='album'>
+							</div>
+							<div class='album-shadow'></div>
+						</div>
+					</a>
+				</div>
+				<div class='col-md-4 col-sm-6'>
+					<a href='#'>
+						<div class='album-wrapper'>
+							<div class='album'>
+								<img src='images/album-cover-sept-2016@2x.jpg' width='325px'>
 							</div>
 							<div class='album-shadow'></div>
 						</div>
@@ -19,21 +36,7 @@
 						</div>
 					</a>
 				</div>
-				<div class='col-md-4'>
-					<a href='#'>
-						<div class='album-wrapper'>
-							<div class='album'></div>
-							<div class='album-shadow'></div>
-						</div>
-					</a>
-					<a href='#'>
-						<div class='album-wrapper'>
-							<div class='album'></div>
-							<div class='album-shadow'></div>
-						</div>
-					</a>
-				</div>
-				<div class='col-md-4'>
+				<div class='col-md-4 col-sm-6'>
 					<a href='#'>
 						<div class='album-wrapper'>
 							<div class='album'></div>
@@ -50,5 +53,6 @@
 			</div>
 		</div>
 	</section>
+         
 
 	<?php include 'footer.php'; ?>
--- playlist-soft-tender-2016.php
+++ playlist-soft-tender-2016.php
@@ -2,7 +2,7 @@
 	<section class='content content-playlist'>
 		<div class='container'>
 			<div class='row'>
-				<div class='col-md-6'>
+				<div class='col-md-4'>
 					<div class='album-wrapper'>
 						<div class='album'>
 							<img src='images/album-cover-soft-tender@2x.jpg', width='325px'>
@@ -10,13 +10,23 @@
 						<div class='album-shadow'></div>
 					</div>
 				</div>
-				<div class='col-md-6'>
-					<iframe src="https://open.spotify.com/embed/user/nattybratty/playlist/0C7qRK2IyVrVFGOlbBi8Ws" width="100%" height="400px" frameborder="0" allowtransparency="true"></iframe>
+				<div class='col-md-7 col-md-offset-1'>
+					<iframe src="https://embed.spotify.com/?uri=spotify%3Auser%3Anattybratty%3Aplaylist%3A21GIP7RGZqwPtMAC9dbrnp" width="100%" height="325" frameborder="0" allowtransparency="true"></iframe>
 				</div>
 			</div>
-			<div class='row'>
-				<div class='col-md-6 col-md-offset-3'>
+			<div class='row playlist-info'>
+				<div class='col-md-4 desc'>
+					<h3>Soft &amp; Tender</h3>
+					<p class='date'>08.2016</p>
+					<p class='desc'>Not constrained to a single genre, this playlist is chicken noodle soup for the tender soul.</p>
+				</div>
+				<div class='col-md-7 col-md-offset-1 track-listings'>
 					<table>
+						<tr>
+							<th>Num:</th>
+							<th>Song:</th>
+							<th>Artist:</th>
+						</tr>
 						<tr>
 							<td>1</td>
 							<td>Interference Fits</td>
